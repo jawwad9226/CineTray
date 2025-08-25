75d9a13b7af052b94bc7ef0aa881751b0028a9e5
SHAIKH JAWWAD AHMAD MOHAMMAD WASIUR RAHMAN <122685975+jawwad9226@users.noreply.github.com>
2025-08-25 18:35:40 +0530

    Refactor service worker event listeners for clarity

--- sw.js
+++ sw.js
@@ -1,20 +1,31 @@
 const CACHE_NAME = 'cinetray-v1.2';
-const STATIC_CACHE_URLS = ['./', './index.html'];
+const STATIC_CACHE_URLS = [
+  './',
+  './index.html'
+];
 
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_CACHE_URLS)));
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_CACHE_URLS))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(cacheNames => Promise.all(
-      cacheNames.map(name => name !== CACHE_NAME ? caches.delete(name) : undefined)
-    ))
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) return caches.delete(cacheName);
+        })
+      );
+    })
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
 });
