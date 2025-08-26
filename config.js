1862022acaec20ba8f9bff04f2e54069ce572de7
jawwad9226shaikhjawwadahmadmomin@gmail.com <you@example.com>
2025-08-26 11:16:31 +0530

    yy

---
+++ config.js
@@ -0,0 +1,6 @@
+// Do NOT commit this file
+window.ENV = {
+  SUPABASE_URL: 'https://srqiupbsyvaqxegfqmlo.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNycWl1cGJzeXZhcXhlZ2ZxbWxvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxNDcxNTMsImV4cCI6MjA3MTcyMzE1M30.dQarMUgOojoohzxNftgfnOJX8hCfUVo2WMsALJNO8Jw',
+  TMDB_API_KEY: '8b6e5d6d4f17264d3c266c79054b6236'
+};
